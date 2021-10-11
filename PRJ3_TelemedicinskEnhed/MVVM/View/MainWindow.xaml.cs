247542abfa299b71c622e4105bac98e3b3855f3d
Søren Mehlsen <202000071@post.au.dk>
2021-10-11 22:55:50 +0200

    Add project files.

---
+++ PRJ3_TelemedicinskEnhed/MVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using RPI;
+using RPI.Controller;
+using RPI.Display;
+using RPI.Heart_Rate_Monitor;
+using RPI.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRJ3_TelemedicinskEnhed
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
